7ff325ebb3290665248a91fb0daaa3cb6d3205d4
CoolLord22 <mastermodmaker@yahoo.com>
2024-12-21 14:55:16 -0500

    Converted Map storage to Set

--- core/src/main/java/com/coollord22/structurecondition/StructureCheck.java
+++ core/src/main/java/com/coollord22/structurecondition/StructureCheck.java
@@ -1,25 +1,30 @@
 package com.coollord22.structurecondition;
 
+import com.gmail.zariust.common.Verbosity;
+import com.gmail.zariust.otherdrops.Log;
 import com.gmail.zariust.otherdrops.event.CustomDrop;
 import com.gmail.zariust.otherdrops.event.OccurredEvent;
 import com.gmail.zariust.otherdrops.parameters.Condition;
 import org.bukkit.Location;
 
-import java.util.Map;
+import java.util.Set;
 
 public abstract class StructureCheck extends Condition {
-    protected final Map<String, Boolean> structureMap;
+    protected final Set<String> structureSet;
 
-    public StructureCheck(Map<String, Boolean> structureMap) {
-        this.structureMap = structureMap;
+    public StructureCheck(Set<String> structureSet) {
+        this.structureSet = structureSet;
     }
 
     @Override
     protected boolean checkInstance(CustomDrop drop, OccurredEvent occurrence) {
-        if (structureMap == null)
+        if (structureSet == null)
             return true;
 
-        // Use NMS to check for structure
+        if (occurrence.getFishingLocation() != null) {
+            Log.logInfo("Fishing location was not null, processing structure check with fishhook location.", Verbosity.HIGH);
+            return isPartOfNaturalStructure(occurrence.getFishingLocation());
+        }
         return isPartOfNaturalStructure(occurrence.getLocation());
     }
 
--- core/src/main/java/com/coollord22/structurecondition/StructureCondition.java
+++ core/src/main/java/com/coollord22/structurecondition/StructureCondition.java
@@ -4,16 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 
 public class StructureCondition extends JavaPlugin {
     public static StructureCheck structureCheck;
+
     static {
         try {
             String packageName = StructureCondition.class.getPackage().getName();
             String internalsName = Bukkit.getServer().getClass().getName().split("\\.")[3];
-            structureCheck = (StructureCheck) Class.forName(packageName + ".StructureCheck" + internalsName).getDeclaredConstructor(Map.class).newInstance((Object) null);
+            structureCheck = (StructureCheck) Class.forName(packageName + ".StructureCheck" + internalsName).getDeclaredConstructor(Set.class).newInstance((Object) null);
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | ClassCastException |
                  NoSuchMethodException | InvocationTargetException exception) {
             Bukkit.getLogger().log(Level.SEVERE, "StructureCondition could not find a valid implementation for this server version.");
