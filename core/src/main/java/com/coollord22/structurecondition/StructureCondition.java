fca8695d4eaf285dac2ed271671c2500e86101f0
CoolLord22 <mastermodmaker@yahoo.com>
2024-12-21 14:55:27 -0500

    Update StructureCondition.java
    Added debug messages

--- core/src/main/java/com/coollord22/structurecondition/StructureCondition.java
+++ core/src/main/java/com/coollord22/structurecondition/StructureCondition.java
@@ -17,19 +17,19 @@ public class StructureCondition extends JavaPlugin {
             structureCheck = (StructureCheck) Class.forName(packageName + ".StructureCheck" + internalsName).getDeclaredConstructor(Set.class).newInstance((Object) null);
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | ClassCastException |
                  NoSuchMethodException | InvocationTargetException exception) {
-            Bukkit.getLogger().log(Level.SEVERE, "StructureCondition could not find a valid implementation for this server version.");
+            Bukkit.getLogger().log(Level.SEVERE, "[ODStructureCondition] could not find a valid implementation for this server version.");
         }
     }
 
     @Override
     public void onEnable() {
-        if(structureCheck != null) {
+        if (structureCheck != null) {
             com.gmail.zariust.otherdrops.parameters.Condition.registerCondition(structureCheck);
+            Bukkit.getLogger().log(Level.INFO, "[ODStructureCondition] Successfully hooked structure condition.");
         }
     }
 
     @Override
     public void onDisable() {
-
     }
 }
